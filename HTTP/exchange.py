679e523ca2a5537250daf757b83eb7c845dd2b07
sefaapinar <sefanur.pinar@gmail.com>
2021-11-06 00:01:40 +0300

    11/06/21

--- HTTP/exchange.py
+++ HTTP/exchange.py
@@ -1,4 +1,16 @@
 import requests
 import json
 
-api_url = ""
+api_url = "http://api.exchangeratesapi.io/latest?base="
+
+bozulan_doviz = input("Bozulan döviz türü: ")
+alinan_doviz = input("Alınan döviz türü: ")
+
+miktar = int(input(f"Ne kadar {bozulan_doviz} bozdurmak istiyorsunuz = "))
+
+result = requests.get(api_url+bozulan_doviz)
+result = json.loads(result.text)
+
+print("1 {0} = {1} {2}".format(bozulan_doviz , result["rates"][alinan_doviz]))
+
+print("{0} {1} = {2} {3} ".format(miktar, bozulan_doviz, miktar * result["rates"][alinan_doviz],alinan_doviz))
