379c20a6dff1d6a745802de4dde8a2f93a4ea61c
sefaapinar <sefanur.pinar@gmail.com>
2021-11-10 11:35:26 +0300

    10/11/21

--- WebScraping/n11-scraping.py
+++ WebScraping/n11-scraping.py
@@ -1,3 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
+url = "https://www.n11.com/bilgisayar/dizustu-bilgisayar"
+
+html = requests.get(url).content
+
+soup = BeautifulSoup(html,"html.parser")
+
+list = soup.find_all("li",{"class":"column"},limit=1)
+
+for li in list:
+    name = li.div.a.h3.text
+    link = li.div.a.get("href")
+    oldprice = li.find("div",{"class":"proDetail"}).find_all("a")[0]
+    newprice = li.find("div",{"class":"proDetail"}).find_all("a")[1]
+
+  
+print(f"name:{name} link: {link} old price:{oldprice} new price:{newprice}")
