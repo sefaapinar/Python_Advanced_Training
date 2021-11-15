bcab1edd87bb38e3870b0ca8974ba154e26db882
sefaapinar <sefanur.pinar@gmail.com>
2021-11-15 12:09:33 +0300

    continue

--- instagram_bot_app/instagram.py
+++ instagram_bot_app/instagram.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 import time
+
+from selenium.webdriver.common import keys
 from userinfo import username,password
 from selenium.webdriver.common.keys import Keys
 
@@ -28,10 +30,7 @@ class Instagram:
 
         time.sleep(3)
 
-        model = self.driver.find_element_by_css_selector("main[role=main] ul")
-        count = len(model.find_element_by_tag_name("li"))
-
-        print(f"takipci sayısı: {count}")
+       
 
 
         if self.driver.find_element_by_class_name('cmbtv'):
@@ -49,21 +48,60 @@ class Instagram:
         time.sleep(1)
 
 
-    def getFollowers(self):
+    def getFollowers(self,max):
 
         self.driver.get(f"https://www.instagram.com/{self.username}")
         time.sleep(2)
         self.driver.find_element_by_class_name("_3dEHb").find_element_by_tag_name("a").click()    
         time.sleep(2)
 
+        model = self.driver.find_element_by_css_selector("main[role=main] ul")
+        count = len(model.find_element_by_tag_name("li"))
+
+
+        action = webdriver.ActionChains(self.driver) #tuşlara tıklama fonksiyonu
+
+
+        print(f"takipci sayısı: {count}")
+
+        while count <max:
+            model.click()
+
+            action.key_down(Keys.SPACE).key_up(Keys.SPACE).perform()
+            time.sleep(2)
+
+            action.key_down(Keys.SPACE).key_up(Keys.SPACE).perform()
+            time.sleep(2)
+
+            action.key_down(Keys.SPACE).key_up(Keys.SPACE).perform()
+            time.sleep(2)
+
+
+            newCount = len(model.find_elements_by_tag_name("li"))
+
+            if count != newCount:
+                count = newCount
+                print(f"takipci sayısı: {count}")
+                time.sleep(1)
+            else:
+                break
+
+       
+
+        i=0
         followers = self.driver.find_element_by_class_name("PZuss").find_elements_by_tag_name("li")
         for user in followers:
+            i+=i
+            print(i)
             link = user.find_element_by_tag_name("a").get_attribute("href")
             print(link)
 
     def followUser(self,username):
         pass
 
+    
+    def followUsers(self,users):
+        pass
 
     def unFollowUser(self,username):
         pass
@@ -77,6 +115,6 @@ class Instagram:
 app = Instagram(username,password)
 
 app.signIn()
-app.getFollowers()
+app.getFollowers(50)
 app.followUser('abc')
 app.unFollowUser('abc')
--- instagram_bot_app/userinfo.py
+++ instagram_bot_app/userinfo.py
@@ -1,2 +1,2 @@
 username="sefaapinar"
-password="1f12fb4fcrx58crx"
+password="x"
