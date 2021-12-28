0b9cde6bfe93b373a601668054badfd55af7f7a1
sefaapinar <sefanur.pinar@gmail.com>
2021-12-28 19:29:05 +0300

    Django App

--- Django/blogapp/blog/models.py
+++ Django/blogapp/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=300)
+    image = models.CharField(max_length=100)
+    description = models.TextField()
+    is_active = models.BooleanField()
+    is_home = models.BooleanField(default=False)
+
+    
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+
+
