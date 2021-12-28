c9447c0b79ce0e99236ec6a45e57cbff68c39dd2
sefaapinar <sefanur.pinar@gmail.com>
2021-12-28 18:30:51 +0300

    Django App

--- Django/blogapp/blog/urls.py
+++ Django/blogapp/blog/urls.py
@@ -1,17 +1,13 @@
 from django.urls import path
 from . import views
-
-
-# https://127.0.0.1:8000/
- # = index.html olabilir
- 
+# http://127.0.0.1:8000/            => homepage
+# http://127.0.0.1:8000/index       => homepage
+# http://127.0.0.1:8000/blogs       => blogs
+# http://127.0.0.1:8000/blogs/3     => blog-details
 
 urlpatterns = [
-    path("",views.index),
-    path("index",views.index),
-    path("blogs",views.blogs),
-    path("blogs/<int:id>", views.blog_details),
-    
-
-
+    path("", views.index, name="home"),
+    path("index", views.index),
+    path("blogs", views.blogs, name="blogs"),
+    path("blogs/<int:id>", views.blog_details, name="blog_details"),
 ]
--- Django/blogapp/blog/views.py
+++ Django/blogapp/blog/views.py
@@ -1,15 +1,59 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render 
+from django.shortcuts import render
+
+data = {
+    "blogs": [
+        {
+            "id": 1,
+            "title": "komple web geliştirme",
+            "image": "1.jpg",
+            "is_active": True,
+            "is_home": False,
+            "description": "çok iyi bir kurs"
+        },
+        {
+            "id": 2,
+            "title": "python kursu",
+            "image": "2.jpg",
+            "is_active": True,
+            "is_home": True,
+            "description": "çok iyi bir kurs"
+        },
+        {
+            "id": 3,
+            "title": "django kursu",
+            "image": "3.jpg",
+            "is_active": False,
+            "is_home": True,
+            "description": "çok iyi bir kurs"
+        }
+    ]
+}
 
 # Create your views here.
-
 def index(request):
-    return render(request,"blog/index.html")
+    context = {
+        "blogs": data["blogs"]
+    }
+    return render(request, "blog/index.html", context)
 
 def blogs(request):
-    return render(request, "blog/blogs.html")
+    context = {
+        "blogs": data["blogs"]
+    }
+    return render(request, "blog/blogs.html", context)
 
 def blog_details(request, id):
-    return render(request,"blog/blog-details.html", {
-        "id":id
+    # blogs = data["blogs"]
+    # selectedBlog = None
+
+    # for blog in blogs:
+    #     if blog["id"] == id:
+    #         selectedBlog = blog
+    blogs = data["blogs"]
+
+    selectedBlog = [blog for blog in blogs if blog["id"]==id][0]
+
+    return render(request, "blog/blog-details.html", {
+        "blog": selectedBlog
     })
