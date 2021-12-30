4efecc63ae5e5ce173364b410ea6cc0afa033063
sefaapinar <sefanur.pinar@gmail.com>
2021-12-31 01:00:53 +0300

    Django App

--- Django/blogapp/blog/urls.py
+++ Django/blogapp/blog/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("", views.index, name="home"),
     path("index", views.index),
     path("blogs", views.blogs, name="blogs"),
+    path("category/<slug:slug>", views.blogs_by_category, name="blogs_by_category"),
     path("blogs/<slug:slug>", views.blog_details, name="blog_details"),
 ]
--- Django/blogapp/blog/views.py
+++ Django/blogapp/blog/views.py
@@ -1,47 +1,21 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from blog.models import Blog
+from blog.models import Blog, Category
 
 
-data = {
-    "blogs": [
-        {
-            "id": 1,
-            "title": "komple web geliştirme",
-            "image": "1.jpg",
-            "is_active": True,
-            "is_home": False,
-            "description": "çok iyi bir kurs"
-        },
-        {
-            "id": 2,
-            "title": "python kursu",
-            "image": "2.jpg",
-            "is_active": True,
-            "is_home": True,
-            "description": "çok iyi bir kurs"
-        },
-        {
-            "id": 3,
-            "title": "django kursu",
-            "image": "3.jpg",
-            "is_active": False,
-            "is_home": True,
-            "description": "çok iyi bir kurs"
-        }
-    ]
-}
 
 # Create your views here.
 def index(request):
     context = {
-        "blogs": Blog.objects.filter(is_active=True, is_home=True)
+        "blogs": Blog.objects.filter(is_active=True, is_home=True),
+        "categories": Category.objects.all()
     }
     return render(request, "blog/index.html", context)
 
 def blogs(request):
     context = {
-        "blogs": Blog.objects.filter(is_active=True)
+        "blogs": Blog.objects.filter(is_active=True),
+        "categories": Category.objects.all()
     }
     return render(request, "blog/blogs.html", context)
 
@@ -54,3 +28,6 @@ def blog_details(request, slug):
     return render(request, "blog/blog-details.html", {
         "blog": blog
     })
+
+def blogs_by_category(request, slug):
+    pass
